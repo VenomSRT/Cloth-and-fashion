cab5626dcdf0de7bb0e6ec06fe379e60c88b048e
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-11-06 23:22:05 +0200

    'finalization'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,17 +15,35 @@ const productsGallery = new Swiper('.products-slider', {
     480: {
       slidesPerView: 3,
     },
+
     640: {
       slidesPerView: 4,
     },
+
     768: {
+      initialSlide: 3,
       slidesPerView: 4.5,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-    }
-  }
+    },
+
+    1080: {
+      initialSlide: 3,
+      slidesPerView: 5.5,
+    },
+
+    1300: {
+      initialSlide: 3,
+      slidesPerView: 6.5,
+    },
+
+    1600: {
+      initialSlide: 3,
+      slidesPerView: 8,
+    },
+  },
 });
 
 const instaGallery = new Swiper('.insta-gallery', {
@@ -37,10 +55,15 @@ const instaGallery = new Swiper('.insta-gallery', {
     480: {
       slidesPerView: 3,
     },
+
     640: {
       slidesPerView: 4.9,
-    }
-  }
+    },
+
+    1260: {
+      slidesPerView: 5,
+    },
+  },
 });
 
 const bottomGallery = new Swiper('.bottom-gallery', {
@@ -52,60 +75,70 @@ const bottomGallery = new Swiper('.bottom-gallery', {
     480: {
       slidesPerView: 2,
     },
+
     640: {
       slidesPerView: 3,
     },
+
     768: {
       slidesPerView: 4,
-    }
-  }
+    },
+
+    1260: {
+      initialSlide: 2,
+      slidesPerView: 4,
+    },
+  },
 });
 
+function Slider(slideSelector, dotSelector, interval) {
+  const sliders = document.querySelectorAll(`.${slideSelector}`);
+  const dots = document.querySelectorAll(`.${dotSelector}`);
 
-const sliders = document.querySelectorAll('.top-slider__slide');
-const dots = document.querySelectorAll('.top-slider__dot');
+  dots.forEach(dot => {
+    dot.addEventListener('click', currentDiv);
+  });
 
-dots.forEach(dot => {
-  dot.addEventListener('click', currentDiv);
-})
+  let slideIndex = 1;
 
-let slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(e) {
-  showDivs(slideIndex = e.target.className[e.target.className.length - 1]);
-}
-
-function showDivs(n) {
-  let i;
-
-  if (n > sliders.length) {
-    slideIndex = 1
-  }
-
-  if (n < 1) {
-    slideIndex = sliders.length
-  }
-
-  for (i = 0; i < sliders.length; i++) {
-    sliders[i].style.opacity = "0";
-  }
-
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" top-slider__dot--active", "");
-  }
-  sliders[slideIndex-1].style.opacity = "1";
-  dots[slideIndex-1].className += " top-slider__dot--active";
-}
-
-setInterval(() => {
   showDivs(slideIndex);
-  if (slideIndex > sliders.length) {
-    slideIndex = 0;
+
+  function currentDiv(e) {
+    showDivs(slideIndex = e.target.className[e.target.className.length - 1]);
   }
-  slideIndex++;
-}, 5000);
+
+  function showDivs(n) {
+    let i;
+
+    if (n > sliders.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = sliders.length;
+    }
+
+    for (i = 0; i < sliders.length; i++) {
+      sliders[i].style.opacity = '0';
+    }
+
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className
+        .replace(` ${dotSelector}--active`, '');
+    }
+    sliders[slideIndex - 1].style.opacity = '1';
+    dots[slideIndex - 1].className += ` ${dotSelector}--active`;
+  }
+
+  setInterval(() => {
+    showDivs(slideIndex);
+
+    if (slideIndex > sliders.length) {
+      slideIndex = 0;
+    }
+    slideIndex++;
+  }, interval);
+}
+
+const topMessages = new Slider('top__message', 'top__dot', 5000);
+const topSlider = new Slider('top-slider__slide', 'top-slider__dot', 4000);
