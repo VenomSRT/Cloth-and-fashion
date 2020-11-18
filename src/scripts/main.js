30ae7d63cb4f936b04125f3a9f25dfb73fb5fd04
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-11-18 13:52:40 +0200

    final commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import Swiper, { Navigation } from 'swiper';
 import 'swiper/swiper-bundle.css';
 
@@ -142,3 +140,71 @@ function Slider(slideSelector, dotSelector, interval) {
 
 const topMessages = new Slider('top__message', 'top__dot', 5000);
 const topSlider = new Slider('top-slider__slide', 'top-slider__dot', 4000);
+
+// Menu button handling
+const menuButton = document.querySelector('.header__menu-burger');
+const menu = document.querySelector('.menu');
+const closeMenuButton = document.querySelector('.menu__cross');
+
+menuButton.addEventListener('click', () => {
+  menu.style.left = '0px';
+});
+
+closeMenuButton.addEventListener('click', () => {
+  menu.style.left = '-100%';
+});
+
+// Menu sizes handling
+const body = document.querySelector('body');
+const menuContainers = document.querySelectorAll('.menu__container');
+let wrapperLeftMargin = window
+  .getComputedStyle(document.querySelector('.wrapper')).marginLeft;
+
+if (parseInt(window.getComputedStyle(body).width) >= 442) {
+  menuContainers.forEach(elem => {
+    elem.style.width = `${parseInt(wrapperLeftMargin) + 442}px`;
+  });
+}
+
+window.addEventListener('resize', () => {
+  if (parseInt(window.getComputedStyle(body).width) >= 442) {
+    wrapperLeftMargin = window
+      .getComputedStyle(document.querySelector('.wrapper')).marginLeft;
+
+    menuContainers.forEach(elem => {
+      elem.style.width = `${parseInt(wrapperLeftMargin) + 442}px`;
+    });
+  } else {
+    menuContainers.forEach(elem => {
+      elem.style.width = '100%';
+    });
+  }
+});
+
+// Submenu handling
+const submenuOpenButtons = document.querySelectorAll('.menu__label');
+
+submenuOpenButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    const submenuList = event.target.nextElementSibling;
+    const submenuItems = submenuList.children;
+
+    if (submenuList.style.display !== 'flex') {
+      submenuList.style.display = 'flex';
+    } else {
+      submenuList.style.display = 'none';
+    }
+
+    if (submenuList.style.display !== 'none') {
+      for (let i = 0; i < submenuItems.length; i++) {
+        setTimeout(() => {
+          submenuItems[i].style.left = '0';
+        }, 250 * (i + 1));
+      };
+    } else {
+      for (let i = 0; i < submenuItems.length; i++) {
+        submenuItems[i].style.left = '-100%';
+      }
+    }
+  });
+});
